d8a6b45d2e5a10edbbf89334353ce87e3814d92f
Tamerlan Khutuev <t.khutuev@gmail.com>
2021-12-24 11:53:02 +0300

    fix2

--- src/components/CreateTask.js
+++ src/components/CreateTask.js
@@ -40,15 +40,15 @@ const CreateTask = ({ getAllTasks }) => {
   };
 
   const popover = {
-    // position: 'absolute',
-    // zIndex: '2',
+    position: 'absolute',
+    zIndex: '2',
   };
   const cover = {
-    // position: 'fixed',
-    // top: '0px',
-    // right: '0px',
-    // bottom: '0px',
-    // left: '0px',
+    position: 'fixed',
+    top: '0px',
+    right: '0px',
+    bottom: '0px',
+    left: '0px',
   };
 
   return (
